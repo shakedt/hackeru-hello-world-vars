d8f274084b3c58e975ff199172c793619116f4ba
shaked <dokuroc@gmail.com>
2023-02-21 16:15:23 +0200

    fix: logs

--- variables.js
+++ variables.js
@@ -1,20 +1,16 @@
-// Variables in JavaScript
+// Variables in JavaScript - https://github.com/shakedt/hackeru-hello-world-vars
+console.log('welcome');
+document.write('welcome');
+// 0. what is variables / מה זה משתנים
 
-// 1. Declaration and Initialization
-// 1. * print or write this variables to the screen
+
+// 1. Declaration and Initialization /  הגדרה והשמה של משתנים// 2. הדפסת משתנים 
 
 
-// 2. Variable Types
-// Variables in JavaScript can be of different types:
-// - String: a sequence of characters.
-// - Number: a numeric value.
-// - Boolean: true or false.
-// - Undefined: a variable that has been declared but not assigned a value.
-// - Null: a variable that has been explicitly assigned no value.
-// - Object: a collection of properties.
-// - Symbol: a unique identifier.
 
-// 3. חוקי שמות משנתים
+// 3. Variable Types - סוגי משתנים
+
+// 4.  Variable Naming Rules  - חוקי שמות משנתים
 
 
 // 4. Changing Variable Values / שינוי ערכי משתנים
@@ -25,15 +21,3 @@
 
 
 // 6. Concatenating Strings / חיבור סטרינגים
-// We can concatenate strings using the + operator.
-
-
-// 7. Outputting Variables
-
-// We can output the value of variables to the console using console.log().
-// console.log(name);
-// document.write("Hello world");
-// console.log(age);
-// console.log(fullName);
-// console.log(result);
-console.log("hello world");
